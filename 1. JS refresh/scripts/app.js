9d87324470b448e780fc8336b18c1b6df6fe12f5
juri931 <impa.mat@hotmail.it>
2024-08-08 11:46:51 +0200

    [add] function example

--- 1. JS refresh/scripts/app.js
+++ 1. JS refresh/scripts/app.js
@@ -9,9 +9,20 @@
 // ------------------------------------------ //
 
 // Import all variables at once
-import * as utils from "./utils";
-console.log(utils.apiKey); // Leggo il valore scelto da utils
-console.log(utils.default); // Leggo il valore default
-console.log(utils.example); // Leggo la seconda variabile
+// import * as utils from "./utils";
+// console.log(utils.apiKey); // Leggo il valore scelto da utils
+// console.log(utils.default); // Leggo il valore default
+// console.log(utils.example); // Leggo la seconda variabile
 
-console.log(utils); // Mi darà il modulo intero e tutto ciò che contiene
+// console.log(utils); // Mi darà il modulo intero e tutto ciò che contiene
+
+// ------------------------------------------ //
+
+// Functions
+function greetUser(userName, message = "Hello!") {
+  // Si può dare un valore ad una variabile direttamente al momento della chiamata
+  console.log(message, userName);
+}
+
+greetUser("Max");
+greetUser("Max", "How are you today?"); // in questo modo sostituirà il valore già dichiarato con un valore custom
