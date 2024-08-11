78caa22af6acea34620856fada73b312825bfae4
juri931 <impa.mat@hotmail.it>
2024-08-11 10:50:25 +0200

    [add] props import w/ shorthand code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-// Image importing
+// Images importing
 import reactImg from "./assets/react-core-concepts.png";
+
+// Props importing
+import { CORE_CONCEPTS } from "./data.js";
 
 // Creating and Using Dynamic Values
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
@@ -14,7 +17,7 @@ function Header() {
 
   return (
     <header>
-      <img src={reactImg} alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" /> {/* Image importing */}
       <h1>React Essentials</h1>
       <p>
         {/* Adding the dynamic value */}
@@ -28,8 +31,37 @@ function Header() {
 function Main() {
   return (
     <main>
-      <h2>Time to get started!</h2>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          {/* Props importing w/ shorthand */}
+          <CoreConcept {...CORE_CONCEPTS[0]} />
+          <CoreConcept {...CORE_CONCEPTS[1]} />
+          <CoreConcept {...CORE_CONCEPTS[2]} />
+          {/* Extended version */}
+          <CoreConcept
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}
+          />
+        </ul>
+      </section>
     </main>
+  );
+}
+
+// Props w/ shorthand
+{
+  /* No more CoreConcept(props) but ({prop1, prop2, prop3}) */
+}
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      {/* No more props.prop_name but just prop_name */}
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
   );
 }
 
