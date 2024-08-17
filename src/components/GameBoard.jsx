d86c00c0311c61708ab8b8fb4f60366996287e09
juri931 <impa.mat@hotmail.it>
2024-08-17 10:43:43 +0200

    [add] lift state up for players and symbols

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,22 +6,29 @@ const initialGameBoard = [
   [null, null, null],
 ]
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard)
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-      updatedBoard[rowIndex][colIndex] = 'X'
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol
       return updatedBoard
     })
+
+    onSelectSquare()
   }
 
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
         <ol>
-          {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
+          {row.map((playerSymbol, colIndex) =>
+            <li key={colIndex}>
+              <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                {playerSymbol}
+              </button>
+            </li>)}
         </ol>
       </li>)}
     </ol>)
