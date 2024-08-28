9725018bdcfaabd2f41cfa8aff9b4f0474f774dc
juri931 <impa.mat@hotmail.it>
2024-08-28 12:39:13 +0200

    "Added react-dom import, and wrapped dialog element with createPortal"

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
-import { h2 } from "fontawesome"
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom"
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef()
@@ -16,15 +16,18 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     }
   })
 
-  return <dialog ref={dialog} className="result-modal">
-    {userLost && <h2>You lost</h2>}
-    {!userLost && <h2>Your score: {score}</h2>}
-    <p>The target time was <strong>{targetTime} seconds</strong></p>
-    <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong></p>
-    <form method="dialog" onSubmit={onReset}>
-      <button>Close</button>
-    </form>
-  </dialog>
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+      <p>The target time was <strong>{targetTime} seconds</strong></p>
+      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong></p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal')
+  )
 })
 
 export default ResultModal
