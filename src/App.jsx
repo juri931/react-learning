e7d274b52df8bed28ef4dd416c7985199356f188
juri931 <impa.mat@hotmail.it>
2024-08-28 11:00:16 +0200

    "Added TimerChallenges component to App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenges from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenges title='Easy' targetTime={1} />
+        <TimerChallenges title='Not easy' targetTime={5} />
+        <TimerChallenges title='Getting Tough' targetTime={10} />
+        <TimerChallenges title='Pros Only' targetTime={15} />
+      </div>
     </>
   );
 }
