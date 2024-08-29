abbcd9a0d7ed3b90a703794d1ec7fbf6537adfbc
juri931 <impa.mat@hotmail.it>
2024-08-29 12:51:38 +0200

    "Added onCancel prop to NewProject component and handleCancelAddProject function to App component"

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ function App() {
     })
   }
 
+  function handleCancelAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      }
+    })
+  }
+
   function handleAddProject(projectData) {
 
     setProjectState(prevState => {
@@ -40,7 +49,7 @@ function App() {
   let content
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Input from "./Input";
 import Modal from "./Modal";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef()
 
   const title = useRef()
@@ -39,8 +39,22 @@ export default function NewProject({ onAdd }) {
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
-          <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-          <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-500 hover:bg-stone-950">Save</button></li>
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-500 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
         </menu>
         <div>
           <Input type='text' ref={title} label='Title' />
