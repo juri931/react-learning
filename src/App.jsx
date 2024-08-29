5ab41453a12bbb0b88c22eb3b3f53afc8d7c8ab9
juri931 <impa.mat@hotmail.it>
2024-08-29 10:42:31 +0200

    fixing vs code bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
