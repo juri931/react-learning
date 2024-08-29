f25259888ec1ce9f1ec08db0ca321dad70df518e
juri931 <impa.mat@hotmail.it>
2024-08-29 09:56:40 +0200

    project change scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import Player from './components/Player.jsx';
-import TimerChallenges from './components/TimerChallenge.jsx';
-
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenges title='Easy' targetTime={1} />
-        <TimerChallenges title='Not easy' targetTime={5} />
-        <TimerChallenges title='Getting Tough' targetTime={10} />
-        <TimerChallenges title='Pros Only' targetTime={15} />
-      </div>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
