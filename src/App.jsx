de8da10dbd474eb5ac3b37c44b3ef83f23608f03
juri931 <impa.mat@hotmail.it>
2024-08-30 11:06:25 +0200

    "Added task management functionality to App component, including handleAddTask and handleDeleteTask functions, and updated NewTask, SelectedProject, and Tasks components to support task creation and deletion."

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,27 @@ function App() {
 
   const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   })
+
+  function handleAddTask(text) {
+    setProjectState(prevState => {
+      const taskId = Math.random()
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      }
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      }
+    })
+  }
+
+  function handleDeleteTask() { }
 
   function handleSelectProject(id) {
     setProjectState(prevState => {
@@ -68,7 +87,14 @@ function App() {
 
   const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  let content =
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectState.tasks}
+    />
 
   if (projectState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
