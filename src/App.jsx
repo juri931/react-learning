357ffa6c0ed86617d5b2c761f9c0267a7ca35535
juri931 <impa.mat@gmail.com>
2024-09-02 10:10:07 +0200

    Scaffolding new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,78 @@
-import { useState } from "react";
-import NewProject from "./components/NewProject";
-import NoProjectSelected from "./components/NoProjectSelected";
-import ProjectsSidebar from "./components/ProjectsSidebar";
-import SelectedProject from "./components/SelectedProject";
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
 
-  const [projectState, setProjectState] = useState({
-    selectedProjectId: undefined,
-    projects: [],
-    tasks: []
-  })
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
 
-  function handleAddTask(text) {
-    setProjectState(prevState => {
-      const taskId = Math.random()
-      const newTask = {
-        text: text,
-        projectId: prevState.selectedProjectId,
-        id: taskId,
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
       }
 
       return {
-        ...prevState,
-        tasks: [newTask, ...prevState.tasks],
-      }
-    })
+        items: updatedItems,
+      };
+    });
   }
 
-  function handleDeleteTask(id) {
-    setProjectState(prevState => {
-      return {
-        ...prevState,
-        tasks: prevState.tasks.filter(task => task.id !== id)
-      }
-    })
-  }
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
 
-  function handleSelectProject(id) {
-    setProjectState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: id,
-      }
-    })
-  }
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
 
-  function handleStartAddProject() {
-    setProjectState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: null,
-      }
-    })
-  }
+      updatedItem.quantity += amount;
 
-  function handleCancelAddProject() {
-    setProjectState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-      }
-    })
-  }
-
-  function handleAddProject(projectData) {
-
-    setProjectState(prevState => {
-      const projectId = Math.random()
-      const newProject = {
-        ...projectData,
-        id: projectId
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
       }
 
       return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: [...prevState.projects, newProject]
-      }
-    })
-
-  }
-
-  function handleDeleteProject() {
-    setProjectState(prevState => {
-      return {
-        ...prevState,
-        selectedProjectId: undefined,
-        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
-      }
-    })
-  }
-
-  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
-
-  let content =
-    <SelectedProject
-      project={selectedProject}
-      onDelete={handleDeleteProject}
-      onAddTask={handleAddTask}
-      onDeleteTask={handleDeleteTask}
-      tasks={projectState.tasks}
-    />
-
-  if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
-  } else if (projectState.selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+        items: updatedItems,
+      };
+    });
   }
 
   return (
-    <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar
-        onStartAddProject={handleStartAddProject}
-        projects={projectState.projects}
-        onSelectProject={handleSelectProject}
-        selectedProjectId={projectState.selectedProjectId}
+    <>
+      <Header
+        cart={shoppingCart}
+        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      {content}
-    </main>
+      <Shop onAddItemToCart={handleAddItemToCart} />
+    </>
   );
 }
 
