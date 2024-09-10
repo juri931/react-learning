b039d6c8ddfb9e03871272e4dc0eaaa3e75f6475
juri931 <impa.mat@gmail.com>
2024-09-10 10:19:55 +0200

    preparing to receive http request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,15 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
