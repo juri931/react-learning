6a897a5fd5f7185d619432b573fdcc81aa03f0a7
juri931 <impa.mat@gmail.com>
2024-09-10 10:40:25 +0200

    "Refactor AvailablePlaces component to use async/await and add isLoading state; update Places component to accept isLoading and loadingText props"

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,22 +4,27 @@ import Places from "./Places.jsx";
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
+  // Using Async/Await
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
