f3245e1106f6f93bef038615b2cb80ea599f3464
juri931 <impa.mat@gmail.com>
2024-09-11 11:42:18 +0200

    "Added input validation and blur handling to Login component"

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,8 +6,10 @@ export default function Login() {
     password: ""
   });
 
-  const emailIsInvalid =
-    enteredValues.email !== "" && !enteredValues.email.includes("@");
+  // Validation
+  const [didEdit, setDidEdit] = useState({ email: false, password: false });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -17,6 +19,11 @@ export default function Login() {
 
   function handleInputChange(identifier, value) {
     setEnteredValues((prevValues) => ({ ...prevValues, [identifier]: value }));
+    setDidEdit((prevEdit) => ({ ...prevEdit, [identifier]: false }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({ ...prevEdit, [identifier]: true }));
   }
 
   return (
@@ -30,6 +37,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
