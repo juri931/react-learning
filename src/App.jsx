55ec522605eda40e3fb1f7e062b43df4b518b2db
juri931 <impa.mat@gmail.com>
2024-09-12 10:23:16 +0200

    "Replace Signup component with Login component in App.jsx, and refactor StateLogin component to use Input component for email and password fields."

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header.jsx";
 // Using StateLogin (useState) for form validation
 import Signup from "./components/Signup";
+// Using Login for code improvement
+import Login from "./components/StateLogin";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Signup />
+        <Login />
       </main>
     </>
   );
